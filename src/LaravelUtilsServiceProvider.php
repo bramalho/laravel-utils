fdc4410b53c27b083eeefc9ab38619baaeba96ef
bruno.ramalho <bruno.ramalho@olx.com>
2018-05-29 19:50:15 +0100

    laravel utils service provider

---
+++ src/LaravelUtilsServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BRamalho\LaravelUtils;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelUtilsServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
